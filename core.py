fc4aee6d41b121bc352a73d5a6c8946f48ea735e
Mathieu Dumas <math.dumas@gmail.com>
2014-11-16 11:30:34 -0500

    premiere ebauche de class starship

--- core.py
+++ core.py
@@ -1,3 +1,8 @@
 __author__ = 'mathieu'
+
+
 class Starship(object):
     """basic starship class"""
+    def __init__(self,starshiptype, starshipsubtype):
+        self.StarshipType = starshiptype
+        self.StarshipSubtype = starshipsubtype
