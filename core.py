853911293d6a37481332e20795fbe81fdd338a06
Mathieu Dumas <math.dumas@gmail.com>
2014-11-17 15:02:47 -0500

    ajout de valeur par defaut d'attributs

--- core.py
+++ core.py
@@ -3,6 +3,11 @@ __author__ = 'mathieu'
 
 class Starship(object):
     """basic starship class"""
-    def __init__(self, starshiptype="ultralight", starshipsubtype="fighter"):
-        self.StarshipType = starshiptype
+    def __init__(self, starshipsubtype="fighter"):
         self.StarshipSubtype = starshipsubtype
+        self.starship = [["Mediumweight", "Cruiser", 7, 5, 5, 30, 200, 6, 3, 2, 2, -8, 6, 720, 28800, 3, 120, 80, 7200, 16, 64, 3],
+                        ["Ultralight", "Fighter", 19, 13, 6, 20, 8, 8, 7, 6, 8, -4, 6, 36, 39, 2, 1, 1, 1700, 8, 48, 3]]
+
+
+unvaisseau = Starship("Cruiser")
+print(unvaisseau.starship[1])
