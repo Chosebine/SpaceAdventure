95ee987fd7521092fb48c4c70e1361c686e64eb2
Mathieu Dumas <math.dumas@gmail.com>
2016-01-09 11:44:57 -0500

    nouveau départ

--- core.py
+++ core.py
@@ -1,1 +1,19 @@
 __author__ = 'mathieu'
+
+class Size(object):
+    """defined size of object"""
+    def __init__(self, name, fightingSquare):
+        self.name = name
+        self.fightingSquare = fightingSquare
+
+class Hull(object):
+    """the hull part of a starship"""
+    def __init__(self, size, name, hitPoint):
+        self.size = size
+        self.name = name
+        self.hitPoint = hitPoint
+
+
+taille = Size("ultralight",0.25)
+ahull= Hull(taille,"vanadium hull",50)
+
