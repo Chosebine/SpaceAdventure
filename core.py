fa8aa04bdeda6196a2e4c495cac7ff41d1617baf
math <math.dumas@gmail.com>
2016-02-25 14:40:26 -0500

    basic rule

--- core.py
+++ core.py
@@ -1,13 +1,16 @@
 __author__ = 'mathieu'
-
+import random
 
 class Size(object):
     """defined size of object"""
 
-    def __init__(self, name, description, fightings_square):
+    def __init__(self, name, size_modif, targeting_system, autopilot, length, weight):
         self.name = name
-        self.description = description
-        self.fightingSquare = fightings_square
+        self.sizeModif = size_modif
+        self.targetingSystem = targeting_system
+        self.autopilot = autopilot
+        self.length = length
+        self.weight = weight
 
 
 class Crew(object):
@@ -45,7 +48,7 @@ class Armor(object):
 class Starship(object):
     """this is a starship object"""
 
-    def __init__(self, name, description, Size, Hull, Armor, equip, posx, posy):
+    def __init__(self, name, description, Size, Hull, Armor, equip, posx, posy,base_speed):
         self.name = name
         self.description = description
         self.size = Size
@@ -54,13 +57,14 @@ class Starship(object):
         self.equip = equip
         self.posx = posx
         self.posy = posy
+        self.baseSpeed = base_speed
 
 
 equipage = Crew("ace", 4, 5, 2, 4, 23)
 
-taille = Size("ultralight", "a very small ship", 0.25)
+taille = Size("huge",-2,5,6,64,256000)
 ahull = Hull("vanadium hull", "this is a basic hull", 50, )
 armortst = Armor("tiberium", "this tiberium armor is blabla", 30, 0.4, -3)
-falcon = Starship("millenium", "the fastest", taille, ahull, armortst, equipage, 4, 5)
+falcon = Starship("millenium", "the fastest", taille, ahull, armortst, equipage, 0, 0,6)
 print(falcon.armor.name)
-print(falcon.equip.quality)
+print(falcon.equip.quality,falcon.size.name)
