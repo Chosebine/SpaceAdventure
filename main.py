7b32d6b8684284f46284d49284bcece2f3903c1b
math <math.dumas@gmail.com>
2016-02-28 12:50:15 -0500

    basic rule

--- main.py
+++ main.py
@@ -14,16 +14,24 @@ sizes = [('Huge', -2, 1, 1, 32, 32000),
          ('Colossal', -8, 3, 3, 256, 1000000),
          ]
 
-starshiptypes = [('Ultralight', 'An ultralight starship can be up to 250 feet long. It occupies a 250-foot-by-250-foot fighting space, and up to four ultralight starships can occupy a single 500-foot square',0.25),
-                ('Light', 'A light starship measures 251–500 feet in length. It has a 500-foot-by-500-foot fighting space and occupies a single 500-foot square', 1),
-                ('Mediumweight', 'A mediumweight starship measures 501–1,000 feet in length. It occupies a 1,000-foot-by-1,000-foot fighting space (4 500-foot squares)', 4),
-                ('Heavy', 'A heavy starship measures 1,001–1,500 feet long. It has a 1,500-foot-by-1,500-foot fighting space (9 500-foot squares).', 9),
-                ('Superheavy', 'A superheavy starship is 1,501 feet long or longer. The smallest superheavy starships (measuring 1,501–2,000 feet long) have a 2,000-foot-by-2,000-foot fighting space (16 500-foot squares), although larger fighting spaces are possible.', 16),
-                ]
-#c.executemany('INSERT INTO starshipType VALUES (?,?,?)', starshiptypes)
+starshiptypes = [('Ultralight', 'An ultralight starship can be up to 250 feet long.'
+                                'It occupies a 250-foot-by-250-foot fighting space,'
+                                ' and up to four ultralight starships can occupy a single 500-foot square', 0.25),
+                 ('Light', 'A light starship measures 251–500 feet in length. '
+                           'It has a 500-foot-by-500-foot fighting space and occupies a single 500-foot square', 1),
+                 ('Mediumweight', 'A mediumweight starship measures 501–1,000 feet in length. '
+                                  'It occupies a 1,000-foot-by-1,000-foot fighting space (4 500-foot squares)', 4),
+                 ('Heavy', 'A heavy starship measures 1,001–1,500 feet long. '
+                           'It has a 1,500-foot-by-1,500-foot fighting space (9 500-foot squares).', 9),
+                 ('Superheavy', 'A superheavy starship is 1,501 feet long or longer. '
+                                'The smallest superheavy starships (measuring 1,501–2,000 feet long) '
+                                'have a 2,000-foot-by-2,000-foot fighting space (16 500-foot squares), '
+                                'although larger fighting spaces are possible.', 16),
+                 ]
+# c.executemany('INSERT INTO starshipType VALUES (?,?,?)', starshiptypes)
 c.execute('SELECT * FROM size')
 print(c.fetchall())
-#for a in range(c.arraysize):
+# for a in range(c.arraysize):
 
 c.execute('SELECT * FROM starshipType')
 
